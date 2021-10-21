ff7c68d205a6d1cceb9394e7308dfe6db58ad55d
Dennis Miyoshi <dennis.miyoshi@hpe.com>
2021-10-21 12:57:16 -0600

    daily commit

--- wetter/getweather.py
+++ wetter/getweather.py
@@ -1,6 +1,5 @@
 import os
 from bs4 import BeautifulSoup
-from getdata import GetData
 from getrawdata import GetRawData
 from getwebdata import GetWebData
 
@@ -8,9 +7,6 @@ def main():
     URL = "https://www.bgc-jena.mpg.de/wetter/weather_data.html"
     '''
     # URL = "http://localhost/wetter/weather_data.html"
-    getdata = GetData(URL)
-    if getdata.webdata_from_table(1, 1, 0) != 0:
-        getdata.get_zip_files()
     '''
     rdata = GetRawData()
     rawdata = rdata.list_rawdata_files()
