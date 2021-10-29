3c185efe5046b304e990bb1b988c5babfe0ab9e1
Dennis Miyoshi <dennis.miyoshi@hpe.com>
2021-10-29 09:15:23 -0600

    added the web crawl side in the code

--- code/golang/getweather.go
+++ code/golang/getweather.go
@@ -2,28 +2,82 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
-    netUrl "net/url"
 
-	"github.com/anaskhan96/soup"
+	"golang.org/x/net/html"
 )
 
 func main() {
-	myRawData := "/Users/dennismiyoshi/github/TimeSeries/RawData"
-	localDataList := listLocalData(myRawData)
-	for i, nm := range localDataList {
-		fmt.Println(i, nm)
+	myRawDataDir := "/Users/dennismiyoshi/github/TimeSeries/RawData"
+	// use locahost with simply pyserver.py to test the code
+	// webcrawl := "http://localhost/wetter/weather_data.html"
+	webcrawl := "https://www.bgc-jena.mpg.de/wetter/weather_data.html"
+	localdataslice := listLocalData(myRawDataDir)
+
+	crawl(webcrawl, localdataslice, myRawDataDir)
+}
+
+func contains(s []string, str string) bool {
+	// search through the slice to see if the string is an element in the slice
+	for _, v := range s {
+		if v == str {
+			// return true if the string is found
+			return true
+		}
 	}
-	// webData, err := GetLatestWebData("http://localhost/wetter/weather_data.html")
-	GetLatestWebData("http://localhost/wetter/weather_data.html")
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// fmt.Println(webData)
+	// return false if the string is not found
+	return false
+}
+
+func getDownloadUrl(uri string, href string) string {
+	u, _ := url.Parse(uri)
+	fmt.Println(u.Host)
+	var validId = regexp.MustCompile(`^[/].*`)
+
+	// if the first character in href is '/' then this is an absolute path
+	// else it's relative
+	if validId.MatchString(href) {
+		u.Path = href
+		return u.String()
+	} else {
+		s := strings.Split(u.Path, "/")
+		if len(s) > 1 {
+			s = s[:len(s)-1]
+		}
+		u.Path = strings.Join(s[:], "/") + "/" + href
+		return u.String()
+	}
+
+}
+
+func getTheFile(href string, uri string, localRawDataDir string) (bool, int) {
+	fname := localRawDataDir + "/" + href
+	out, err := os.Create(fname)
+	if err != nil {
+		return false, 0
+	}
+	defer out.Close()
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		return false, 0
+	}
+	defer resp.Body.Close()
+	n, err := io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Println("Error copying to file")
+		return false, 0
+	} else {
+		return true, int(n)
+	}
 }
 
 func listLocalData(path string) []string {
@@ -46,27 +100,65 @@ func listLocalData(path string) []string {
 	}
 }
 
-//func GetLatestWebData(url string) (string, error) {
-func GetLatestWebData(url string) {
-	var ref []string
-    u, errr := netUrl.Parse(url)
-    if errr != nil {
-        os.Exit(1)
-    }
-    fmt.Println(u)
-	// Get the HTML
-	resp, err := soup.Get(url)
+func getHref(t html.Token) (ok bool, href string) {
+	// Iterate over token attributes until we find an "href"
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+	// "bare" return will return the variables (ok, href) as
+	// defined in the function definition
+	return
+}
+
+// Extract all 'a' href links from a given webpage
+func crawl(uri string, localdataslice []string, localRawDataDir string) {
+	resp, err := http.Get(uri)
+
 	if err != nil {
+		fmt.Println("ERROR: Failed to crawl:", uri)
 		os.Exit(1)
 	}
 
-	doc := soup.HTMLParse(resp)
-	links := doc.FindAll("a")
-	for _, link := range links {
-		// fmt.Println(link.Text(), "| Link :", link.Attrs()["href"])
-		if strings.Contains(link.Attrs()["href"], "mpi_roof") {
-			ref = append(ref, link.Attrs()["href"])
+	b := resp.Body
+	defer b.Close() // close Body when the function completes
+
+	var validId = regexp.MustCompile(`^(/mpi_roof_2|mpi_roof_2).*(.zip)$`)
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+
+		if tt == html.ErrorToken {
+			err := z.Err()
+			if err == io.EOF {
+				//end of the file, break out of the loop
+				break
+			}
+			// There was an error tokenizing,
+			// which likely means the HTML was malformed.
+			log.Fatalf("error tokenizing HTML: %v", z.Err())
+		}
+
+		if tt == html.StartTagToken {
+			t := z.Token()
+			// Check if the token is an <a> tag
+			if isAnchor := t.Data == "a"; isAnchor {
+				// Extract the href value, if there is one
+				if ok, href := getHref(t); ok {
+					if validId.MatchString(href) && !contains(localdataslice, href) {
+						fmt.Println("Downloading", href)
+						success, fsize := getTheFile(href, getDownloadUrl(uri, href), localRawDataDir)
+						if success {
+							fmt.Printf("Successfully retrieved %s with size %d", href, fsize)
+						} else {
+							fmt.Println("Something failed", fsize)
+						}
+					}
+				}
+			}
 		}
 	}
-	fmt.Println(len(ref))
 }
