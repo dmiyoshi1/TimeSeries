6c5caaaa9fd2986fe654dee681ec66adce57cc24
Dennis Miyoshi <dennis.miyoshi@hpe.com>
2021-11-03 09:58:28 -0600

    add second method to concatenate csv files

--- code/python/cleandata.py
+++ code/python/cleandata.py
@@ -38,9 +38,25 @@ def main():
         if '.csv' in file:
             csvlist.append(file)
     csvlist.sort()
-
+    '''
+    # This is the pandas concat method. However this is slower than the direct
+    # Python method of concatenating a list of files.
     df = pd.concat([pd.read_csv(f, encoding='iso-8859-1') for f in csvlist])
     df.to_csv(tensorDataDir + '/roof.csv', index=False, encoding='utf-8')
+    '''
+
+    count = 0
+    with open(tensorDataDir + "/roof.csv","w", encoding='utf-8') as fout:
+        for file in csvlist:
+            if count == 0:
+                count = 1
+                with open(file, "r", encoding='iso-8859-1') as f:
+                    fout.write(f.read())
+            else:
+                # now the rest:    
+                with open(file, "r", encoding='iso-8859-1') as f:
+                    next(f) # skip the header
+                    fout.write(f.read())
 
 if __name__ == "__main__":
     main()
